e7ecf87e7cd08865b8ceec1d1a3e6e535ad250ef
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-12-12 20:27:00 +0400

    Update HeroSection.tsx

--- components/Home/HeroSection/HeroSection.tsx
+++ components/Home/HeroSection/HeroSection.tsx
@@ -1,15 +1,15 @@
 // components/Home/HeroSection/HeroSection.tsx
 
 import React from "react";
-import styles from "./HeroSection.module.css"; // Import styles from module.css
-import Image from 'next/image'; // Import Image component from Next.js
+import styles from "./HeroSection.module.css";
+import Image from 'next/image';
 
 const HeroSection: React.FC = () => {
     return (
         <div className={styles.heroSection}>
             <div className={styles.textWrapper}>AKASH SASIKUMAR</div>
             <div className={styles.frame} />
-            <Image className={styles.line} alt="Line" src="/line-2.svg" width={1440} height={2} />
+            <Image className={styles.line} alt="Line" src="assets/Line.svg" width={1440} height={2} />
         </div>
     );
 };
