ce467d3d9076a116d7700f1bbcbcf9f3d10b4bfc
AkashSasikumar47 <akashsasikumar47@gmail.com>
2023-12-20 18:00:29 +0400

    Update HeroSection.tsx

--- components/Home/HeroSection/HeroSection.tsx
+++ components/Home/HeroSection/HeroSection.tsx
@@ -6,8 +6,8 @@ const HeroSection: React.FC = () => {
     return (
         <div className={styles.heroSection}>
             <div className={styles.textWrapper}>AKASH SASIKUMAR</div>
-            <div className={styles.frame} />
-            <Image className={styles.line} alt="Line" src="assets/Line.svg" width={1440} height={2} />
+            <Image className={styles.customImage} alt="Custom Image" src="/assets/images/Hero_Image.png" layout="responsive" width={500} height={300} />
+            <Image className={styles.line} alt="Line" src="/assets/Line.svg" width={1440} height={2} />
         </div>
     );
 };
